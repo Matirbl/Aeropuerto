74fa7e04db585ff4bbaf93f36f6e1545849ab102
Mati <mati.rbl@gmail.com>
2022-01-21 18:32:52 -0300

    Empezando

--- src/AeropuertoFinal/Aeropuerto.java
+++ src/AeropuertoFinal/Aeropuerto.java
@@ -1,38 +1,50 @@
 package AeropuertoFinal;
 
+import java.util.Queue;
 import java.util.concurrent.Semaphore;
 
 public class Aeropuerto {
     private FreeShop freeShop;
+    private int hall;
     private Reloj reloj;
     private Semaphore mutexIngreso;
     private Semaphore mutexSalida;
+    private Queue puesto1;           //puesto de cada aerolinea
+    private Queue puesto2;
+    private Queue puesto3;
+
+
+
 
     private Aeropuerto(Reloj r) {           //Constructor
         mutexIngreso = new Semaphore(1);
         mutexSalida = new Semaphore(1);
         reloj = r;
+        hall = 500;
     }
+
 
 
     public void abrir() {
         try {
             System.out.println("********ABRE AEROPUERTO********");
-           /* puedenSubirALosColes();
+            freeShop.setAbierto(true);
+            /*
             carrera.setAbierto(true);
             faro.setAbierto(true);
             laguna.setAbierto(true);
-            shopping.setAbierto(true);
             abrirPiletas();
             abrio.release(100);//importante que tenga correspondencia con el main*/
         } catch (Exception e) {
         }
     }
 
-    public void ingresar(Persona p) {
+    public void ingresar(Persona p, int aerolinea) {
         try {
             mutexIngreso.acquire();
             System.out.println(p.getIdPersona() + " INGRESA AL AEROPUERTO " + reloj.obtenerHora());
+            //trata de ingresar a su puesto de atención
+            //si no hay lugar espera en el hall
             mutexIngreso.release();
         } catch (Exception e) {
         }
--- src/AeropuertoFinal/Persona.java
+++ src/AeropuertoFinal/Persona.java
@@ -4,16 +4,18 @@ public class Persona implements Runnable {
 
     private int idPersona;
     private Aeropuerto aeropuerto;
+    private int idAerolinea;
 
 
     public Persona(int id, Aeropuerto recursoCompartido) {
         idPersona = id;
         aeropuerto = recursoCompartido;
+
     }
 
     @Override
     public void run() {
-        aeropuerto.ingresar(this);
+        aeropuerto.ingresar(this, idAerolinea);
         //hacer actividades
         aeropuerto.salir(this);
     }
